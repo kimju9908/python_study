4f4d2bf39e20545acccc50bf65fa87710015c807
김주혁 <kimj9908@naver.com>
2024-09-12 16:43:35 +0900

    test commit

---
+++ 입출력.py
@@ -0,0 +1,31 @@
+# a,b,c = map(int,input("3자리 정수를 입력해주세요 ").split(" "))
+# d=str(a*b*c)
+#
+# for e in range(10):
+#     if d.count(f"{e}") >0 :
+#         print(d.count(f"{e}"))
+#     else:
+#         if d.count(f"{e}") == 0 :
+#             print("0")
+# for i in range(10):
+#     print(d.count(str(i)), end = "" )
+
+
+#
+# a= input("문자열을 입력하세요")
+#
+# for e in range(len(a)-1, -1 , -1):
+#     print(a[e] , end = "")
+
+# n= int(input("정수를 입력하세요: "))
+# for e in range(n):
+#     for i in range(e+1):
+#         print("*",end = "")
+#     print( )
+
+a  = "dasdwasd"
+print(a[::])
+
+
+
+
